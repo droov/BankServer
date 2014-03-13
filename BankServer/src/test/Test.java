5fdaad209fc54b0c96d647186fdbb6bbab373b0e
droov <dhruv.bindal@outlook.com>
2014-03-13 20:28:48 +0800

    Yep, I think it worked

--- BankServer/src/test/Test.java
+++ BankServer/src/test/Test.java
@@ -6,8 +6,9 @@ public class Test {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 		System.out.println("Test 2");
-		System.out.println("Test 6");
+		System.out.println("Test 3");
 		System.out.println("Test 7");
+		System.out.println("Hello, please work");
 	}
 
 }

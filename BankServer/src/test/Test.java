1e4f75b370c41b160b1e6921536fa1e521443bcd
KanishkKanoria <kanishk.kanoria@gmail.com>
2014-03-13 20:21:38 +0800

    Testing 4

--- BankServer/src/test/Test.java
+++ BankServer/src/test/Test.java
@@ -6,7 +6,7 @@ public class Test {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 		System.out.println("Test 2");
-		System.out.println("Test 3");
+		System.out.println("Test 4");
 		System.out.println("Test 7");
 	}
 
